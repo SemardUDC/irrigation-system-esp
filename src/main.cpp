bc30124322456db15f775ef7d0fae87cd6dea04b
Humberto Leal <humberto.adolfo@hotmail.com>
2018-11-27 16:21:14 -0500

    ActionManager con struct SolenoidValve para manejo de grupos de electrovalvulas

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <ArduinoJson.h>
 #include <Arduino.h>
 #include "PCF8574.h"
+// Managers
+#include <ActionManager.h>
 // Sensors
 #include <UltrasonicPCF8574.h>
 #include <FlowMeter.h>
@@ -22,78 +24,71 @@ PubSubClient mqtt_client(espClient);
 // PCF8574 initialization
 PCF8574 pcf20(0x20, D2, D1);
 
-// Ultrasonic initialization
-const uint8_t TRIGGER_PIN = 0;
-const uint8_t ECHO_PIN = 1;
-void ultr_sensor_callback(unsigned int current_distance)
-{
-    Serial.print("Distance: ");
-    Serial.println(current_distance);
-    //Creates a JSON to send to topic containing
-    //ultr info.
-    StaticJsonBuffer<200> jsonBuffer;
-    JsonObject &root = jsonBuffer.createObject();
-    root["identification"] = "666";
-    root["description"] = "test from sketch";
-    root["distance"] = current_distance;
-    String message;
-    root.printTo(message);
-    Serial.print("JSON generated: ");
-    Serial.println(message);
-    mqtt_client.publish("irrigation-system/sensor/ultrasonic", message.c_str());
-}
-UltrasonicPCF8574 ultr(TRIGGER_PIN, ECHO_PIN, 600, ultr_sensor_callback, pcf20);
-
-// Flow Meter initialization
-const uint8_t flow_signal_pin = D5;
-void flow_meter_callback(unsigned int current_content)
-{
-    static int oldTime = 0;
-    if (millis() - oldTime > 10000)
-    {
-        StaticJsonBuffer<200> jsonBuffer;
-        JsonObject &root = jsonBuffer.createObject();
-        root["identification"] = 666;
-        root["description"] = "tanque vaciado";
-        root["content"] = current_content;
-        oldTime = millis();
-        String message;
-        root.printTo(message);
-        Serial.print("JSON generated: ");
-        Serial.println(message);
-        mqtt_client.publish("irrigation-system/sensor/water-flow", message.c_str());
-    }
-}
-
-FlowMeter flowM1(flow_signal_pin, flow_meter_callback);
-
 // Tests the PCF by blinking a LED
 // every second.
 int old_time_test_pcf = 0;
 bool state_test_pcf = true;
 const uint8_t pin_test_pcf = 3;
 
-// Solenoid-valve initialization
-void solenoid_callback(uint8_t state)
+// Creates a json message for publishing to a solenoid-valve mqtt topic.
+String createJsonMessage(uint8_t identification, char *description, char *key, bool val)
 {
     StaticJsonBuffer<200> jsonBuffer;
     JsonObject &root = jsonBuffer.createObject();
-    root["identification"] = 666;
-    root["description"] = "electrovalvula 1";
-    root["open"] = state ? true : false;
+    root["identification"] = identification;
+    root["description"] = description;
+    root[key] = val;
     String message;
     root.printTo(message);
-    Serial.print("JSON generated: ");
-    Serial.println(message);
-    mqtt_client.publish("irrigation-system/actuator/solenoid-valve/get", message.c_str());
+    return message;
 }
-Electrovalvula solenoid1(3, pcf20);
+
+const int VALVES_SIZE = 5;
+
+// Array of Electrovalvula objects.
+Electrovalvula valves[VALVES_SIZE] = {
+    {0, pcf20}, {1, pcf20}, {2, pcf20}, {3, pcf20}, {4, pcf20}};
+
+// Array of callbacks.
+void (*valves_callbacks[VALVES_SIZE])(uint8_t state) = {
+    [](uint8_t state) {
+        String message = createJsonMessage(0, "electrovalvula 1", "open", (state ? true : false));
+        mqtt_client.publish("irrigation-system/actuator/solenoid-valve/get", message.c_str());
+    },
+    [](uint8_t state) {
+        String message = createJsonMessage(1, "electrovalvula 2", "open", (state ? true : false));
+        mqtt_client.publish("irrigation-system/actuator/solenoid-valve/get", message.c_str());
+    },
+    [](uint8_t state) {
+        String message = createJsonMessage(2, "electrovalvula 3", "open", (state ? true : false));
+        mqtt_client.publish("irrigation-system/actuator/solenoid-valve/get", message.c_str());
+    },
+    [](uint8_t state) {
+        String message = createJsonMessage(3, "electrovalvula 4", "open", (state ? true : false));
+        mqtt_client.publish("irrigation-system/actuator/solenoid-valve/get", message.c_str());
+    },
+    [](uint8_t state) {
+        String message = createJsonMessage(4, "electrovalvula 5", "open", (state ? true : false));
+        mqtt_client.publish("irrigation-system/actuator/solenoid-valve/get", message.c_str());
+    }};
+
+// Array of SolenoidValve.
+SolenoidValve sol_valves[VALVES_SIZE] = {
+    {valves[0], *valves_callbacks[0]},
+    {valves[1], *valves_callbacks[1]},
+    {valves[2], *valves_callbacks[2]},
+    {valves[3], *valves_callbacks[3]},
+    {valves[4], *valves_callbacks[4]}
+};
+
+// ActionManager object that manages 5 SolenoidValve
+// variables.
+ActionManager actuators(sol_valves, VALVES_SIZE);
 
 void setup()
 {
     Serial.begin(9600);
     pcf20.begin();
-    flowM1.begin();
 
     delay(10);
     Serial.println();
@@ -137,35 +132,16 @@ void loop()
                 // ... and resubscribe
                 mqtt_client.subscribe("inTopic");
                 mqtt_client.subscribe("irrigation-system/actuator/solenoid-valve/set");
-                // Callback for managing the activation/deactivation of a solenoid-valve.
+                // Callback for managing the receiving mqtt messages.
                 mqtt_client.setCallback([](char *topic, uint8_t *payload, unsigned int length) {
                     String solenoid_topic = "irrigation-system/actuator/solenoid-valve/set";
                     Serial.println("Mensaje recibido para topic: ");
-                    Serial.println(solenoid_topic);
+                    Serial.println(topic);
+                    // Evaluates if the topic for the message received
+                    // is from solenoid-valve/set
                     if (solenoid_topic.equals(topic))
                     {
-                        StaticJsonBuffer<200> jsonBuffer;
-                        JsonObject &message = jsonBuffer.parseObject(payload);
-                        Serial.println("Mensaje recibido:");
-                        String jsonmessage;
-
-                        message.printTo(jsonmessage);
-                        Serial.println(jsonmessage.c_str());
-
-                        if (solenoid1.getPin() == message["identification"])
-                        {
-                            Serial.println("electrovalvula encontrada.");
-                            if (message["state"] == "open")
-                            {
-                                Serial.println("Abriendo electrovalvula.");
-                                solenoid1.abrir(solenoid_callback);
-                            }
-                            else if (message["state"] == "close")
-                            {
-                                Serial.println("Cerrando electrovalvula.");
-                                solenoid1.cerrar(solenoid_callback);
-                            }
-                        }
+                        actuators.handleValveMessage((char *)payload);
                     }
                     else
                     {
@@ -186,17 +162,4 @@ void loop()
         }
     }
     mqtt_client.loop();
-    if (millis() - old_time_test_pcf > 10000)
-    {
-        old_time_test_pcf = millis();
-        // As the ultrsonic sensor obtains the distance
-        // parameter directly, is not necessary to handle
-        // it every time.
-        ultr.handle();
-    }
-    // The readings received from flowMeter are more
-    // critical to capture, because it needs the pulses
-    // to calculate the content passed, that's why on
-    // every loop the handle is called.
-    flowM1.handle();
 }
