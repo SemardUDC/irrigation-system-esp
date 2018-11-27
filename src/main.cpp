3f21e508adfa41ffebb8c489f9a0a0f5e9ea81a8
Humberto Leal <humberto.adolfo@hotmail.com>
2018-11-27 16:21:14 -0500

    ActionManager con struct SolenoidValve para manejo de grupos de electrovalvulas

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "PCF8574.h"
 // Managers
 #include "SensorManager.h"
+#include <ActionManager.h>
 // Sensors
 #include <UltrasonicPCF8574.h>
 #include <FlowMeter.h>
@@ -52,31 +53,76 @@ void (*ultrasonic_callbacks[ULTRASONICS_SIZE])(unsigned int current_distance){
 
 // Array of ultrasonic sensors with PCF8574.
 UltrasonicPCF8574 ultras[ULTRASONICS_SIZE] = {
-    {0, 1, 600, ultrasonic_callbacks[0], pcf20},
-    {2, 3, 600, ultrasonic_callbacks[1], pcf20}};
+    {4, 5, 600, ultrasonic_callbacks[0], pcf20},
+    {6, 7, 600, ultrasonic_callbacks[1], pcf20}};
 
-const int FLOW_METERS_SIZE = 2;
+const int FLOW_METERS_SIZE = 1;
 
 // Array of callbacks for flow meter sensors.
 void (*flowm_callbacks[FLOW_METERS_SIZE])(unsigned int current_content) = {
     [](unsigned int current_content) {
         String message = createJsonMessage(1, "flow_1", "content", current_content);
         mqtt_client.publish("irrigation-system/sensor/water-flow", message.c_str());
-    },
-    [](unsigned int current_content) {
-        String message = createJsonMessage(2, "flow_2", "content", current_content);
-        mqtt_client.publish("irrigation-system/sensor/water-flow", message.c_str());
     }};
 
 // Array of flow meter sensors.
 FlowMeter flows[FLOW_METERS_SIZE] = {
-    {D5, flowm_callbacks[0]},
-    {D6, flowm_callbacks[1]}};
+    {D5, flowm_callbacks[0]}};
 
 // SensorManager object that manages
 // the array of FlowMeter and Ultrasonic
 // sensors.
-SensorManager sensors(flows, 2, ultras, 2);
+SensorManager sensors(flows, FLOW_METERS_SIZE, ultras, ULTRASONICS_SIZE);
+
+// Creates a json message for publishing to a solenoid-valve mqtt topic.
+String createJsonMessage(uint8_t identification, char *description, char *key, bool val)
+{
+    StaticJsonBuffer<200> jsonBuffer;
+    JsonObject &root = jsonBuffer.createObject();
+    root["identification"] = identification;
+    root["description"] = description;
+    root[key] = val;
+    String message;
+    root.printTo(message);
+    return message;
+}
+
+const int VALVES_SIZE = 4;
+
+// Array of Electrovalvula objects.
+Electrovalvula valves[VALVES_SIZE] = {
+    {0, pcf20}, {1, pcf20}, {2, pcf20}, {3, pcf20}};
+
+// Array of callbacks.
+void (*valves_callbacks[VALVES_SIZE])(uint8_t state) = {
+    [](uint8_t state) {
+        String message = createJsonMessage(0, "electrovalvula 1", "open", (state ? true : false));
+        mqtt_client.publish("irrigation-system/actuator/solenoid-valve/get", message.c_str());
+    },
+    [](uint8_t state) {
+        String message = createJsonMessage(1, "electrovalvula 2", "open", (state ? true : false));
+        mqtt_client.publish("irrigation-system/actuator/solenoid-valve/get", message.c_str());
+    },
+    [](uint8_t state) {
+        String message = createJsonMessage(2, "electrovalvula 3", "open", (state ? true : false));
+        mqtt_client.publish("irrigation-system/actuator/solenoid-valve/get", message.c_str());
+    },
+    [](uint8_t state) {
+        String message = createJsonMessage(3, "electrovalvula 4", "open", (state ? true : false));
+        mqtt_client.publish("irrigation-system/actuator/solenoid-valve/get", message.c_str());
+    }};
+
+// Array of SolenoidValve.
+SolenoidValve sol_valves[VALVES_SIZE] = {
+    {valves[0], *valves_callbacks[0]},
+    {valves[1], *valves_callbacks[1]},
+    {valves[2], *valves_callbacks[2]},
+    {valves[3], *valves_callbacks[3]}
+};
+
+// ActionManager object that manages 5 SolenoidValve
+// variables.
+ActionManager actuators(sol_valves, VALVES_SIZE);
 
 void setup()
 {
@@ -127,6 +173,25 @@ void loop()
                 mqtt_client.publish("outTopic", "hello world");
                 // ... and resubscribe
                 mqtt_client.subscribe("inTopic");
+                mqtt_client.subscribe("irrigation-system/actuator/solenoid-valve/set");
+                // Callback for managing the receiving mqtt messages.
+                mqtt_client.setCallback([](char *topic, uint8_t *payload, unsigned int length) {
+                    String solenoid_topic = "irrigation-system/actuator/solenoid-valve/set";
+                    Serial.println("Mensaje recibido para topic: ");
+                    Serial.println(topic);
+                    // Evaluates if the topic for the message received
+                    // is from solenoid-valve/set
+                    if (solenoid_topic.equals(topic))
+                    {
+                        actuators.handleValveMessage((char *)payload);
+                    }
+                    else
+                    {
+                        Serial.print("Topic: ");
+                        Serial.print(topic);
+                        Serial.println(" not contemplated.");
+                    }
+                });
             }
             else
             {
